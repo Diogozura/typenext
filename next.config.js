12fe7c38348ce2e6a5bf1cf95edb4747242ac7cb
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-06-18 00:53:44 -0300

    novo teste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+   
+   
 }
 
 module.exports = nextConfig
